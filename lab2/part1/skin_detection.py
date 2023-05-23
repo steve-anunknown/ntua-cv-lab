d953ee62161ef16c41510111dcdbaf766666999d
steve-anunknown <steve.anunknown@gmail.com>
2023-05-23 21:27:20 +0300

    they seem to be working now.

--- lab2/part1/skin_detection.py
+++ lab2/part1/skin_detection.py
@@ -1,16 +1,36 @@
-import skin_detection_utils
-import numpy as np
+from skin_detection_utils import FitSkinGaussian
+from skin_detection_utils import fd
+import matplotlib.pyplot as plt
+import scipy.io
 import cv2
 
 if __name__ == "__main__":
-    # load some image from the dataset in
-    # the "part1-GreekSignLanguage" folder.
-
-    image = cv2.imread("part1-GreekSignLanguage/1/1_1.jpg")
-
     # load the skin samples that are stored in the
     # same folder in .mat format in order to fit
     # a gaussian curve on them.
+    skin_samples = scipy.io.loadmat("part1-GreekSignLanguage/skinSamplesRGB.mat")
+    skin_samples = skin_samples["skinSamplesRGB"]
+    # first convert the rgb values to ycbcr
+    skin_samples = cv2.cvtColor(skin_samples, cv2.COLOR_RGB2YCR_CB)
+    # fit a gaussian curve on the skin samples
+    mu, cov = FitSkinGaussian(skin_samples)
+    print(f"Mean: {mu}")
+    print(f"Covariance: {cov}")
+    for i in range(1, 15):
+        # load some image from the dataset in
+        # the "part1-GreekSignLanguage" folder.
+        image = cv2.imread(f"part1-GreekSignLanguage/{i}.png")
 
-    skin_samples = np.load("part1-GreekSignLanguage/skinSamplesRGB.mat")
+        # convert the image to YCbCr color space
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2YCR_CB)
+        boundaries = fd(image, mu, cov)
+        # draw the bounding boxes on the image
+        for boundary in boundaries:
+            x, y, w, h = boundary
+            cv2.rectangle(image, (y, x), (y+h, x+w), (0, 0, 255), 2)
+        # show the image
+        fig, axs = plt.subplots(1, 1)
+        axs.imshow(image)
+        plt.show()
+        
     
--- lab2/part1/skin_detection_utils.py
+++ lab2/part1/skin_detection_utils.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy import stats
 from scipy import ndimage
 
-SKIN_THRESHOLD = 0.1
+SKIN_THRESHOLD = 0.05
 
 def FitSkinGaussian(ycbcr_image):
     """ Return mean and covariance of interest points' gaussian distribution
@@ -14,10 +14,9 @@ def FitSkinGaussian(ycbcr_image):
     cbcr_image = ycbcr_image[:, :, 1:3]
     height, width, channels = cbcr_image.shape
     # calculate the average of cb and cr channels
-    mu = np.mean(cbcr_image[:, :, :], axis=0)
+    mu = np.mean(cbcr_image.reshape(height*width, channels), axis=0)
     # calculate the covariance of cb and cr channels
-    cov = np.cov(cbcr_image[:, :, 0].flatten(), cbcr_image[:, :, 1].flatten())
-
+    cov = np.cov(cbcr_image.reshape(height*width, channels).T)
     return (mu, cov)
 
 def fd(image, mean, covariance):
@@ -28,32 +27,36 @@ def fd(image, mean, covariance):
     mean -- the mean value of the gaussian distribution.
     covariance -- the covariance of the gaussian distribution.
     """
-
-    distribution = scipy.stats.multivariate_normal(mean, covariance)
+    # define the gaussian distribution according to the mean and covariance
+    distribution = stats.multivariate_normal(mean, covariance)
     
     # dstack means depth stack
     # we give the Cb channel and the Cr channel
     # and it forms an array of vectors (Cb, Cr)
     pixels = np.dstack((image[:,:,1], image[:,:,2]))
-    skin_image = (distribution.pdf(pixels) >= SKIN_THRESHOLD).astype(int)
+    # for the thresholding to work, it seems to be necessary to
+    # normalize the probability. i'm not sure why.
+    skin_image = (distribution.pdf(pixels)/np.max(distribution.pdf(pixels)) >= SKIN_THRESHOLD).astype('uint8')
 
     # the skin image probably has holes
     # we will attempt to close them by performing
     # the opening with a small structural element and
     # the closing with a bigger structural element
-    opening_strel = np.ones((3,3))
-    closing_strel = np.ones((9,9))
+    opening_strel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    closing_strel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (19, 19))
     skin_image = cv2.morphologyEx(skin_image, cv2.MORPH_OPEN, opening_strel)
     skin_image = cv2.morphologyEx(skin_image, cv2.MORPH_CLOSE, closing_strel)
-    labels, features = np.ndimage.label(skin_image)
+    # find the connected components
+    # and return the bounding boxes of the
+    # connected components
+    labels, features = ndimage.label(skin_image)
     boundaries = []
-    for label in labels:
-        x, y = np.where(features == label)
-        x = x.sort()
-        y = y.sort()
-        width = x[-1] - x[0]
-        height = y[-1] - y[0]
-        boundaries.append( x[0], y[0], width, height)
+    for label in range(1, features+1):
+        # find the bounding box of the feature
+        # and append it to the list of boundaries
+        indices = np.where(labels == label)
+        x, y = indices[0], indices[1]
+        boundaries.append((np.min(x), np.min(y), np.max(x)-np.min(x), np.max(y)-np.min(y)))
     return boundaries
 
 
