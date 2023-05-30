c8913e2cc88d91cce05c1ce03b60ff573d8f19ad
steve-anunknown <steve.anunknown@gmail.com>
2023-05-30 13:40:56 +0300

    trying to correctly setup main

--- lab2/part1/tracking.py
+++ lab2/part1/tracking.py
@@ -8,13 +8,26 @@ import scipy.io
 import cv2
 
 if __name__ == "__main__":
-    initial = cv2.imread("part1-GreekSignLanguage/1.png")
-    initial = cv2.cvtColor(initial, cv2.COLOR_BGR2YCR_CB)
+    original = cv2.imread("part1-GreekSignLanguage/1.png")
+    initial = cv2.cvtColor(original, cv2.COLOR_BGR2YCR_CB)
     skin_samples = scipy.io.loadmat("part1-GreekSignLanguage/skinSamplesRGB.mat")
     skin_samples = skin_samples["skinSamplesRGB"]
     skin_samples = cv2.cvtColor(skin_samples, cv2.COLOR_RGB2YCR_CB)
     mu, cov = FitSkinGaussian(skin_samples)
     boundaries = fd(initial, mu, cov)
+    # find good features to track from the first image
+    # in each boundary
+    gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+    features = []
+    initial_guesses = []
+    for boundary in boundaries:
+        x, y, w, h = boundary
+        cropped = gray[x:x+w, y:y+h]
+        feats = cv2.goodFeaturesToTrack(cropped, 25, 0.01, 10)
+        feats = np.squeeze(feats.astype(np.int32))
+        features.append(feats)
+        initial_guesses.append(np.array([np.zeros(len(feats)), np.zeros(len(feats))]))
+    
     for i in range(1, 20):
         # load some image from the dataset in
         # the "part1-GreekSignLanguage" folder.
@@ -22,37 +35,33 @@ if __name__ == "__main__":
         image2 = cv2.imread(f"part1-GreekSignLanguage/{i+1}.png")
         gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
         gray2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
-        # convert the image to YCbCr color space
-        image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2YCR_CB)
-        image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2YCR_CB)
 
-
-        for boundary in boundaries:
+        for index, boundary in enumerate(boundaries):
             x, y, w, h = boundary
             # crop the image to retain only the pixels
             # inside of the boundaries
             cropped1 = gray1[x:x+w, y:y+h]
             cropped2 = gray2[x:x+w, y:y+h]
-            features = cv2.goodFeaturesToTrack(cropped2, 25, 0.01, 10)
-            # initial estimation of movement
-            # set to zero because we have no estimation
-            dx0, dy0 = np.zeros(len(features)), np.zeros(len(features))
-            # use squeeze to remove the redundant dimension
-            features = np.squeeze(features.astype(np.int32))
-            [dx, dy] = lk(cropped1, cropped2, features, 1, 0.001, dx0, dy0)
 
 
-            [flowx, flowy] = displ(dx, dy, "energy")
-            # update the initial guess
-            dx0 = dx
-            dy0 = dy    
+            dx0, dy0 = initial_guesses[index]
+            print(initial_guesses[index].shape)
+            [dx, dy] = lk(cropped1, cropped2, features[index], 2, 0.04, dx0, dy0)
+            # update the guesses
+            initial_guesses[index] = np.array([dx, dy])
+            # update the features
+            features[index] = features[index] + np.array(list(zip(dx, dy)))
+
+
+            # compute the flow
+            [flowx, flowy] = displ(dx, dy, 0.1) 
             # update the boundaries
-            x = round(x + flowx)
-            y = round(y + flowy)
+            x = int(x + flowx)
+            y = int(y + flowy)
+            boundaries[boundaries.index(boundary)] = (x, y, w, h)
             # show the new boundaries
             cv2.rectangle(image2, (y, x), (y+h, x+w), (0, 0, 255), 2)
             plt.quiver(-dx, -dy, angles='xy', scale=100)
-
 
         # show the image
         fig, axs = plt.subplots(1, 1)
