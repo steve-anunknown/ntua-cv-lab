d4292b2f06f60678ba044e1d0898f11f73aa4036
steve-anunknown <steve.anunknown@gmail.com>
2023-06-10 13:33:39 +0300

    slight changes

--- lab2/part2/gabor.py
+++ lab2/part2/gabor.py
@@ -4,11 +4,13 @@ from cv23_lab2_2_utils import read_video
 from cv23_lab2_2_utils import show_detection
 
 if __name__ == "__main__":
-    num_frames = 200
+    num_frames = 100
     # get video name from the command line
     if len(sys.argv) > 1:
         video_name = sys.argv[1]
     video = read_video(video_name, num_frames, 0)
-    gabor_points = GaborDetector(video, sigma=4, tau=1.5, threshold=0.25)
+    gabor_points = GaborDetector(video, sigma=3, tau=1.5,
+                                 threshold=0.25, num_points=500)
     print("Gabor points: ", gabor_points.shape)
+    print("Gabor points: ", gabor_points)
     show_detection(video, gabor_points, "Gabor Detector")
--- lab2/part2/harris.py
+++ lab2/part2/harris.py
@@ -5,10 +5,12 @@ from cv23_lab2_2_utils import show_detection
 
 
 if __name__ == "__main__":
-    num_frames = 200
+    num_frames = 100
     if len(sys.argv) > 1:
         video_name = sys.argv[1]
     video = read_video(video_name, num_frames, 0)
-    harris_points = HarrisDetector(video, s=2, sigma=4, tau=1.5, kappa=0.005, threshold=0.2)
+    harris_points = HarrisDetector(video, s=2, sigma=3, tau=1.5,
+                                   kappa=0.005, threshold=0.05, num_points=500)
     print("Harris points: ", harris_points.shape)
+    print("Harris points: ", harris_points)
     show_detection(video, harris_points, "Harris Detector")
